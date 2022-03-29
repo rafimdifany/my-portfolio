f6c3c37fb341f3c707b8bebd3adfc8df83ae0691
Rafi Mufadhal Difany <rafimdifany@gmail.com>
2022-03-29 23:18:13 +0700

    Coloring Profile name text & add clip path as background design

--- src/components/profile-component/left-profile.component/left-profile.component.jsx
+++ src/components/profile-component/left-profile.component/left-profile.component.jsx
@@ -3,8 +3,8 @@ import { Component } from "react";
 class LeftProfile extends Component {
   render() {
     return (
-      <div className="w-1/2 p-32">
-        <h1 className="text-5xl mb-5 font-bold">Rafi Mufadhal Difany</h1>
+      <div className="w-1/2 pt-60 pl-24">
+        <h1 className="text-6xl mb-5 font-bold text-sky-500">Rafi Mufadhal Difany</h1>
         <h2>Software Engineer</h2>
         <h2>Web Developer</h2>
         <h2>UI/UX Designer</h2>
--- src/components/profile-component/profile.component.jsx
+++ src/components/profile-component/profile.component.jsx
@@ -1,12 +1,14 @@
 import { Component, Fragment } from "react";
 import LeftProfile from "./left-profile.component/left-profile.component";
 import RightProfile from "./right-profile-component/right-profile.component";
+import "./profile.component.css";
 
 class Profile extends Component {
   render() {
     return (
       <Fragment>
-        <div className="flex bg-gray-100 h-screen">
+        <div className="shape h-full w-full bg-sky-500 absolute">asdws</div>
+        <div className="flex bg-grey-100 h-screen">
           <LeftProfile />
           <RightProfile />
         </div>
