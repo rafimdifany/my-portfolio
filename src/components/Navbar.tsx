acbc422dedcd0f647843ad572d7585b2c2aed758
rafimdifany <rafimdifany@gmail.com>
2025-07-09 23:16:57 +0700

    adding assets & create simple navbar
    - remove default assets
    - adding assets (icon)
    - create simple navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+export default function Navbar() {
+    return (
+        <nav className="bg-gray-800 p-4">
+        <div className="container mx-auto flex justify-between items-center">
+            <div className="text-white text-lg font-semibold">Rafi</div>
+            <ul className="flex space-x-4">
+                <li>
+                    <a href="/" className="text-gray-300 hover:text-white"><span className="text-purple-500">#</span>home</a>
+                </li>
+                <li>
+                    <a href="/portfolio" className="text-gray-300 hover:text-white"><span
+                        className="text-purple-500">#</span>works</a>
+                </li>
+                <li>
+                    <a href="/about" className="text-gray-300 hover:text-white"><span
+                        className="text-purple-500">#</span>about-me</a>
+                </li>
+                <li>
+                    <a href="/contact" className="text-gray-300 hover:text-white"><span
+                        className="text-purple-500">#</span>contacts</a>
+                </li>
+            </ul>
+        </div>
+        </nav>
+    );
+}
